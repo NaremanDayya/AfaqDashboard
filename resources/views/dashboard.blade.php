c6bab513405dfe6cb42ac98be990d1cb05c10163
NaremanDayya <nanadaya166@gmail.com>
2026-01-20 11:48:01 +0200

    messages translation file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ Auth::user()->isAdmin() ? __('messages.test') : __('messages.daily_logs') }}
+            {{ Auth::user()->isAdmin() ? __('messages.admin_dashboard') : __('messages.daily_logs') }}
         </h2>
     </x-slot>
 
